3944f08f9c3b7d07abbbf8ebbdfb31a74e152452
1570599338@qq.com <1570599338@qq.com>
2017-12-03 22:58:25 +0800

    返回数据是list类型

---
+++ src/com/lquan/dao/test/Test4_1.java
@@ -0,0 +1,50 @@
+package com.lquan.dao.test;
+
+import java.io.Reader;
+import java.util.List;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import com.lquan.test.models.Post;
+import com.lquan.test.models.ReadUser;
+import com.lquan.test.models.User;
+
+/**
+ * 
+ * <p>Title:       Test1</p>
+ * <p>Description: 测试mybatis模块</p>
+ * <p>Created by  lquan on 2017年11月30日下午4:15:01 </p>
+ *<p>@email:  </p>
+ */
+public class Test4_1 {
+	private static SqlSessionFactory sqlSessionFactory;
+	private static Reader reader;
+	
+	static {
+		try {
+			reader = Resources.getResourceAsReader("config/Configure4_1.xml");
+			sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	public static SqlSessionFactory getSession() {
+		return sqlSessionFactory;
+	}
+
+	public static void main(String[] args) {
+		SqlSession session = sqlSessionFactory.openSession();
+		List<User> user = session.selectList("com.lquan.test.models.getUserList", "lq"); 
+	     System.out.println("用户："+user.toString());
+//                 List<Post> posts = user.getPosts();  
+                 for(User p : user) {
+                     System.out.println("post:"+p.toString()); 
+                 }
+	}
+
+}
